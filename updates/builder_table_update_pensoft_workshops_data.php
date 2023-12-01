3b044c309bf1990509e71a3d06f96dd31be073b0
root <root@project-dev.pensoft.local>
2023-12-01 09:36:15 +0200

    composer and new field

---
+++ updates/builder_table_update_pensoft_workshops_data.php
@@ -0,0 +1,23 @@
+<?php namespace Pensoft\Workshops\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftWorkshopsData extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_workshops_data', function($table)
+        {
+            $table->string('place')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_workshops_data', function($table)
+        {
+            $table->dropColumn('place');
+        });
+    }
+}
